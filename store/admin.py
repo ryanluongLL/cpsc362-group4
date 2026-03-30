67d9db108d669dc9c10f77679799a106e51013fe
beth-le <elizabethdle@csu.fullerton.edu>
2026-03-30 11:00:06 -0700

    Added databases for user account, user profile, address, and future payment token storage for Stripe.

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Category, Product
+from .models import Address, Category, PaymentMethodToken, Product, UserAccount, UserProfile
 
 admin.site.register(Category)
 admin.site.register(Product)
+admin.site.register(UserAccount)
+admin.site.register(UserProfile)
+admin.site.register(Address)
+admin.site.register(PaymentMethodToken)
